4c2683077a2978f9e40aaff84028f75103759866
bigfu@home-pc <bigfu1993@163.com>
2024-03-27 12:50:45 +0800

    useEffect 依赖项

--- src/Effect/index.js
+++ src/Effect/index.js
@@ -1,16 +1,33 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
-function init(){
+function init() {
     console.log('init fn')
 }
 
 function Effect() {
+    const [str, setStr] = useState('abc')
+    const [num, setNum] = useState(1)
+    function effectFn() {
+        console.log('effect fn')
+    }
+    // 初始化执行一次
     useEffect(() => {
         console.log('use effact is running')
         init()
+        effectFn()
     }, [])
+    // 视图更新
+    useEffect(() => {
+        console.log('view is change')
+    })
+    // 依赖项更新
+    useEffect(() => {
+        console.log('str is change')
+    }, [str])
     return (<>
-        <h1>use effect</h1>
+        <h1>use effect </h1>
+        <div onClick={() => setNum(Math.random(8))}>点击更新组件视图:{num}</div>
+        <div onClick={() => setStr(`${str}*`)}>点击更新str：{str}</div>
     </>)
 }
 
