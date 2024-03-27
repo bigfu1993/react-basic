20e65143b2c49cc1d0ef2d684ba9e968505affa8
bigfu@home-pc <bigfu1993@163.com>
2024-03-28 01:50:47 +0800

    react-router query传参

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,5 +1,13 @@
-export default function Article(){
+import { Link, useSearchParams } from 'react-router-dom'
+export default function Article() {
+    const [params] = useSearchParams()
+    const id = params.get('id')
+    const name = params.get('name')
     return (
-        <div>Article</div>
+        <>
+            <div>Article</div>
+            <Link to="/login">to="login"</Link>
+            <div>id:{id} | name:{name}</div>
+        </>
     )
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,11 @@
-export default function Login(){
+import { Link, useNavigate } from 'react-router-dom'
+export default function Login() {
+    const navigate = useNavigate()
     return (
-        <div>Login</div>
+        <>
+            <div>Login</div>
+            <Link to="/article?id=1&name=ls">to="article</Link>
+            <div onClick={() => navigate('/article?id=2&name=zs')}>点击跳转</div>
+        </>
     )
 }
