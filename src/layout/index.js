44c30b80aef76348342b0c5040cc922c6664821a
bigfu@home-pc <bigfu1993@163.com>
2024-04-08 23:24:18 +0800

    redux async

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,15 +1,27 @@
 import { Outlet, Link } from "react-router-dom";
-import { useSelector,useDispatch } from 'react-redux'
-import {inscrement,addToNum} from '../store/modules/counterStore'
+import { useSelector, useDispatch } from 'react-redux'
+import { inscrement, addToNum } from '../store/modules/counterStore'
+import { fetchChannlList } from '../store/modules/channelStore'
+import { useEffect } from "react";
 
 export default function Layout() {
     const { count } = useSelector(state => state.counter)
     const dispatch = useDispatch()
+
+    const { channelList } = useSelector(state => state.channel)
+    useEffect(() => {
+        dispatch(fetchChannlList())
+    }, [dispatch])
     return (
         <>
             <h1>layout : {count}</h1>
-            <button onClick={()=>dispatch(inscrement())}>count +1</button>
-            <button onClick={()=>dispatch(addToNum(10))}>count +10</button>
+            <button onClick={() => dispatch(inscrement())}>count +1</button>
+            <button onClick={() => dispatch(addToNum(10))}>count +10</button>
+            <div>
+                <ul>
+                    {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+                </ul>
+            </div>
             <div>
                 <h2>router link</h2>
                 <Link to='/jack'>to jack</Link> \
